8e69142359d8cd639dac251a904bf22f0ddc712a
Tomas <tomas@planetary-tech.se>
2019-03-28 21:51:23 +0100

    asd

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-router.get("/api", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+router.post("/api", (req, res) => {
+  console.log(req.body);
 });
 module.exports = router;
