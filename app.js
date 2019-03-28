f26a6c4f965882b050af98ee2ea57df56fab6c2d
Tomas <tomas@planetary-tech.se>
2019-03-28 20:06:13 +0100

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+const app = express();
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server);
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => getApiAndEmit(socket),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+const getApiAndEmit = async socket => {
+  try {
+   
+    socket.emit("FromAPI", "hello world");
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+server.listen(port, () => console.log(`Listening on port ${port}`));
